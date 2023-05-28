4b00afcbee31a62753f56a6c89a517751adc7690
laerrylaessig <laerrylaessig@gmail.com>
2023-05-28 10:43:12 +0200

    extract tab registration in own file

---
+++ tab_registration.js
@@ -0,0 +1,9 @@
+const tabController = new TabController();
+        
+const MicrocontrollerContent = document.getElementById("tab-microcontroller-content");
+const MicrocontrollerButton = document.getElementById("tab-microcontroller-button");
+tabController.registerTab("tab-microcontroller", MicrocontrollerContent, MicrocontrollerButton);
+
+const SolarmoduleContent = document.getElementById("tab-solarmodule-content");
+const SolarmoduleButton = document.getElementById("tab-solarmodule-button");
+tabController.registerTab("tab-solarmodule", SolarmoduleContent, SolarmoduleButton);
